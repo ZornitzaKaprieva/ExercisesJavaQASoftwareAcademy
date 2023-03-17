be14b9b4fdfeabaf289fb0093a8b2c9cb684af0d
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-03-17 14:03:00 +0200

    P01indexMultipliedBy5 added.

---
+++ src/main/java/homework/arrays/P01indexMultipliedBy5.java
@@ -0,0 +1,19 @@
+package homework.arrays;
+
+//from Lecture 7
+
+/* Да се напише програма, която създава масив с 20 елемента от целочислен тип
+и инициализира всеки от елементите със стойност равна на индекса на елемента умножен по 5.
+Елементите на масива да се изведат на конзолата. */
+public class P01indexMultipliedBy5 {
+    public static void main(String[] args) {
+
+        int[] to20Answer = new int[20];
+        int i = 0;
+
+        for (i = 0; i < to20Answer.length; i++) {
+            to20Answer[i] = i * 5;
+            System.out.println(to20Answer[i]);
+        }
+    }
+}
