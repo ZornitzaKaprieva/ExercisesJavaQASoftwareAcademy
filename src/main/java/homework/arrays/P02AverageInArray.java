089b22b14f175f9341fc39e13b65606309969f99
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-26 11:54:30 +0300

    P02AverageInArray

--- src/main/java/homework/arrays/P02AverageInArray.java
+++ src/main/java/homework/arrays/P02AverageInArray.java
@@ -3,9 +3,9 @@ package homework.arrays;
 import java.util.Scanner;
 
 /*Предстои Вело състезание за благотворителност в което участниците са разпределени в младша("juniors") и старша("seniors") група. Парите се набавят от таксата за участие на велосипедистите. Според възрастовата група и вида на трасето на което ще се провежда състезанието, таксата е различна.
-Група	  trail	        cross-country	downhill	road
-juniors 	5.50	           8	    12.25	        20
-seniors	    7	            9.50	    13.75	        21.50
+Група	    trail    cross-country    downhill    road
+juniors 	5.50	  8	              12.25	      20
+seniors	    7	      9.50	          13.75	      21.50
 Ако в "cross-country" състезанието се съберат 50 или повече участника(общо младши и старши), таксата  намалява с 25%. Организаторите отделят 5% процента от събраната сума за разходи.
 Вход
 От конзолата се четат 2 числа и един стринг, всяко на отделен ред:
@@ -14,10 +14,11 @@ seniors	    7	            9.50	    13.75	        21.50
 •	Третият ред – вид трасе – "trail", "cross-country", "downhill" или "road"
 Изход
 Да се отпечата на конзолата едно число:
-"{дарената сума}" - форматирана с точност до 2 знака след десетичната запетая.
-*/
+"{дарената сума}" - форматирана с точност до 2 знака след десетичната запетая.*/
+
 public class P02AverageInArray {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int juniors = Integer.parseInt(scanner.nextLine()); //[1…100]
@@ -65,7 +66,6 @@ public class P02AverageInArray {
 
         double expenses = payTax * 0.05;
         double moneyForCharity = payTax - expenses;
-
         System.out.printf("%.2f", moneyForCharity);
     }
 }
