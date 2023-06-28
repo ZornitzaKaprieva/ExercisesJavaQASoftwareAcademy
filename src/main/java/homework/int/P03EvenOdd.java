c9fc6a25597c06c86f9456010230c0e80e1c0f9c
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-28 14:39:29 +0300

    P03EvenOdd

--- src/main/java/homework/int/P03EvenOdd.java
+++ src/main/java/homework/int/P03EvenOdd.java
@@ -1,10 +1,9 @@
 package homework03Int;
 
-// from Lecture 06:
-
 import java.util.Scanner;
 
 /* Напишете програма/ метод, която чете число и изписва в конзолата дали числото е четно или не? */
+
 public class P03EvenOdd {
     public static void main(String[] args) {
 
@@ -17,14 +16,13 @@ public class P03EvenOdd {
         }
 
         //solution 02:
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number to find out if it's even: ");
-        int number = Integer.parseInt(scanner.nextLine());
-        if (number % 2 == 0) {
-            System.out.println("This number is even.");
-        } else {
-            System.out.println("This number is odd.");
-        }
-
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter a number to find out if it's even: ");
+        // int number = Integer.parseInt(scanner.nextLine());
+        // if (number % 2 == 0) {
+        //     System.out.println("This number is even.");
+        // } else {
+        //     System.out.println("This number is odd.");
+        // }
     }
 }
